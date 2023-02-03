067bc1220a5d6239e3bc909886b8e95c779233f2
Michael Chan <michaelalexanderchan@gmail.com>
2023-02-02 19:57:56 -0500

    Deleted redundant js files

--- src/ListController.js
+++ src/ListController.js
@@ -1,8 +1,23 @@
 import toDo from './ToDoObject';
 import toDoList from './ToDoList';
 
-// Create our list to store To-Do objects
+const hello = () => {
+  const container = document.getElementById('container');
+  const header = document.createElement('h1');
+  header.textContent = 'Bonjour';
+  container.appendChild(header);
+
+  document.body.appendChild(container);
+};
+
+hello();
+
+const object1 = toDo('wash dishes', 'wash the darn dishes', 'today', 'high', 'you can use the dishwasher');
+console.log(object1);
 const listObject = toDoList();
+listObject.addToDo(object1);
+console.log(listObject);
+
 const pageDisplay = document.getElementById('container');
 
 const displayLists = () => {
